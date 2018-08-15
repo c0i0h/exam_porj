7e5bf036b33cc16a706cb6c1eea1a90a7dba64d3
c0i0h <c0i0h@naver.com>
2018-08-16 08:22:41 +0900

    기능 보완

--- study/src/ch07/extend/ExtendExam.java
+++ study/src/ch07/extend/ExtendExam.java
@@ -11,17 +11,26 @@ public class ExtendExam {
 	}
 	public static void main(String[] args) {
 		new ExtendExam();
-		new Child();
-		new Child2();
-		new GrandChild();
+		Child c1 = new Child();
+		Child2 c2 = new Child2();
+		GrandChild gc = new GrandChild();
+		GrandChild2 gc2 = new GrandChild2();
 		
 		Other other = new Other();
-		System.out.println("- ExtendExam 객체는 Other 객체를 포함한다");
+		System.out.println("- ExtendExam 객체는 Other 객체를 포함한다.");
+		
+		//c1.printMe();
+		//c2.printMe();
+		//gc.printMe();
+		//gc2.printMe();
 	}
 
 }
 class Parent {
 	int age = 0;
+	void printMe() {
+		System.out.println("Parent 의 printMe() 메소드~");
+	}
 }
 class Child extends Parent {
 	Child(){
@@ -32,6 +41,11 @@ class Child2 extends Parent {
 	Child2() {
 		System.out.println("- Child2 클래스는 age를 가지고 있지 않으나 Parent에게 상속받은 age=" +super.age);
 	}
+	
+	@Override
+	void printMe() {
+		System.out.println("Child2 의 printMe() 메소드~");
+	}
 }
 
 class GrandChild extends Child {
@@ -41,6 +55,10 @@ class GrandChild extends Child {
 		System.out.println("하지만 Child 클래스가 Parent에게 상속받은 age가 있기에  Child 클래스에는  age 멤버 변수가 있는 것으로 간주한다.");
 		System.out.println("그러므로 GrandChild 클래스는 age를 사용할수 있게 된다.");
 	}
+}
+
+class GrandChild2 extends Child2 {
+	
 }
 
 class Other extends Object {
