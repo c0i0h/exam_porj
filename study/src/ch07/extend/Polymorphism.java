40c984e268fbfa9724f06435efdcf0d83e576a96
c0i0h <c0i0h@naver.com>
2018-08-17 14:02:07 +0900

    기능 확장

--- study/src/ch07/extend/Polymorphism.java
+++ study/src/ch07/extend/Polymorphism.java
@@ -8,10 +8,30 @@ public class Polymorphism {
 
 	Polymorphism() {
 		Tv t = new Tv();
+		t.power = true;
+		t.channel++;
+		t.power();
+		t.channelUp();
+		t.channeldown();
 		
 		CaptionTv c = new CaptionTv();
+		c.power = true;
+		c.channel++;
+		c.power();
+		c.channelUp();
+		c.channeldown();
+		// ÀÌÇÏ CaptionTv °´Ã¼ Àü¿ë ¸â¹ö
+		c.text = "";
+		c.caption();
 		
 		Tv tv = new CaptionTv();
+		tv.power = true;
+		tv.channel++;
+		tv.power();
+		tv.channelUp();
+		tv.channeldown();
+		//tv.text = "";		// error, text cannot be resolved or is not a field
+		//tv.caption();		// error, caption(), The method caption() is undefined for the type Tv
 	}
 	
 	public static void main(String[] args) {
